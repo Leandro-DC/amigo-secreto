75257a92075f3f8e63aec93dee66ebe78308f88d
Leandro-DC <leandcx94@gmail.com>
2025-09-23 15:28:36 -0300

    Inicio proyecto agregando array solicitada

---
+++ app.js
@@ -0,0 +1,5 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//CREAR UN ARRAY PARA ALMACENAR LOS NOMBRES
+/*Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados.*/
+
+let amigos = [];
