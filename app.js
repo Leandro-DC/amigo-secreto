522afe0345c79c045404133d30f9738416aa8a4e
Leandro-DC <leandcx94@gmail.com>
2025-09-23 22:35:01 -0300

    Agrego funcion actualizarLista y sortearAmigo, funcionando

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//CREAR UN ARRAY PARA ALMACENAR LOS NOMBRES
+//CREAR UN ARRAY PARA ALMACENAR LOS NOMBRES.
 /*Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados.*/
 
 let amigos = [];
 
-//IMPLEMENTA UNA FUNCIÓN PARA AGREGAR AMIGOS
+//IMPLEMENTA UNA FUNCIÓN PARA AGREGAR AMIGOS.
+
 /*CAPTURAR EL VALOR DEL CAMPO DE ENTRADA: Utilizar document.getElementById o document.querySelector para 
 obtener el texto ingresado por el usuario.
 -agrego .value para dar valor de string.
@@ -32,9 +33,61 @@ function agregarAmigo() {
     amigos.push(nombreIngresado);
     console.log(amigos);
     limpiarCaja();
+    actulizarLista();
     return;
 }
 
 function limpiarCaja() {
     let valorCaja = document.getElementById("amigo").value = "";
+}
+
+//IMPLEMENTA UNA FUNCIÓN PARA ACTUALIZAR LA LISTA DE AMIGOS.
+
+/*OBTENER EL ELEMENTO DE LA LISTA: Utilizar document.getElementById() o document.querySelector() para 
+seleccionar la lista donde se mostrarán los amigos.*/
+
+/*LIMPIAR LA LISTA EXISTENTE: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados 
+al actualizar.*/
+
+/*ITERAR SOBRE EL ARREGLO: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) 
+para cada título.*/
+
+/*AGREGAR ELEMENTOS A LA LISTA: Para cada amigo, crear un nuevo elemento de lista.*/
+
+function actulizarLista() {
+    let listaDeAmigos = document.getElementById("listaAmigos");
+    listaDeAmigos.innerHTML = "";
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        listaDeAmigos.appendChild(li);
+    }
+    return;
+}
+
+//IMPLEMENTA UNA FUNCIÓN PARA SORTEAR LOS AMIGOS
+
+/*VALIDAR QUE HAYA AMIGOS DISPONIBLES: Antes de sortear, comprobar si el array amigos no está vacío.*/
+
+/*GENERAR UN ÍNDICE ALEATORIO: Usar Math.random() y Math.floor() para seleccionar un índice aleatorio 
+del arreglo.*/
+
+/*OBTENER EL NOMBRE SORTEADO: Utilizar el índice aleatorio para acceder al nombre correspondiente en 
+el arreglo.*/
+
+/*MOSTRAR EL RESULTADO: Actualizar el contenido del elemento de resultado utilizando document.getElementById()  
+e innerHTML para mostrar el amigo sorteado.*/
+
+function sortearAmigo() {
+
+    if (amigos.length === 0) {
+        return;
+    }
+
+    let sorteoAmigo = Math.floor(Math.random()*amigos.length);
+    let amigoSecreto = amigos[sorteoAmigo];
+
+    let resultadoSorteo = document.getElementById("resultado");
+    resultadoSorteo.innerHTML = `Tu amigo secreto es ${amigoSecreto}!`;
 }
